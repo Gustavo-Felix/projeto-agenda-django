988936a44c460a8719e4eaf2ed7cdad3127116e0
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-04-11 10:12:38 -0300

    Criação do Primeiro Model - Django

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin # type:ignore
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    ...
