f0d22abfda7b776fb08bbee2532fe80adfc68f01
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-04-14 10:10:57 -0300

    Iniciando nas Views

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,16 @@
 from django.db import models # type:ignore
 from django.utils import timezone # type:ignore
+from django.contrib.auth.models import User # type:ignore
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Contact(models.Model):
     
@@ -9,6 +20,20 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,  
+        )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,  
+        )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
