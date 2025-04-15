8602d79de07fe8ee4a4548fa93da5af851674e26
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-04-15 11:00:37 -0300

    Adicionando o Header

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render # type:ignore
+from django.shortcuts import get_object_or_404, render # type:ignore
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos'
     }
 
     return render(
@@ -12,4 +13,17 @@ def index(request):
         'contact/index.html',
         context
 
-    )
+)
+
+def contact(request, contact_id):
+    singleContact = get_object_or_404(Contact, pk=contact_id, show=True)
+    context = {
+        'contact': singleContact,
+        'site_title': singleContact.first_name
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+)
